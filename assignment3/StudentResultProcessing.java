d87cea16e7575ca3836fd244c71fbfbe1f69466a
Rishabh Pandey <rishabhp0608@gmail.com>
2025-08-19 19:15:08 +0530

    q6: completed

--- assignment3/StudentResultProcessing.java
+++ assignment3/StudentResultProcessing.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 abstract class Student {
     protected  int rollNo;
     protected  String name;
@@ -46,5 +48,39 @@ class PostgraduateStudent extends Student {
 }
 
 public class StudentResultProcessing {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Student []students = new Student[5];
+
+        for (int i =0; i < students.length; i++) {
+            System.out.println("\nEnter information for student-" + (i+1));
+            System.out.print("Enter the type of student (1 for UG, 2 for PG): ");
+            int type = scanner.nextInt();
+
+            if (type != 1 && type != 2) {
+                System.out.println("Not a valid type! Try again!");
+                i--;
+                continue;
+            }
+
+            System.out.print("Enter the roll number for student: ");
+            int rollNo = scanner.nextInt();
+            scanner.nextLine();
+            System.out.print("Enter the name for student: ");
+            String name = scanner.nextLine();
+            System.out.print("Enter the marks for " + name.toUpperCase() + ": ");
+            int marks = scanner.nextInt();
+
+            if (type == 1) {
+                students[i] = new UndergraduateStudent(rollNo, name, marks);
+            } else {
+                students[i] = new PostgraduateStudent(rollNo, name, marks);
+            }
+        }
+        scanner.close();
+
+        for (Student student : students) {
+            student.calculateResult();
+        }
+    }
 }
